0e5a54d8aae4c4201fb28a75442975630ab4a52a
hmorrow4 <147641009+hmorrow4@users.noreply.github.com>
2025-11-22 14:10:18 -0600

    Create Insert_Enrollment_Script.sql

---
+++ Insert_Enrollment_Script.sql
@@ -0,0 +1,44 @@
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (1, 1, 1, '2024-01-15');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (2, 1, 3, '2024-01-15');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (3, 2, 2, '2024-01-16');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (4, 2, 4, '2024-01-16');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (5, 3, 1, '2024-01-18');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (6, 3, 5, '2024-01-18');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (7, 4, 2, '2024-01-20');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (8, 4, 6, '2024-01-20');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (9, 5, 3, '2024-01-22');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (10, 5, 7, '2024-01-22');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (11, 6, 4, '2024-01-23');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (12, 6, 8, '2024-01-23');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (13, 7, 5, '2024-01-25');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (14, 8, 6, '2024-01-26');
+
+INSERT INTO Enrollment (EnrollmentID, StudentID, CourseID, Enrollment_Date)
+VALUES (15, 9, 7, '2024-01-27');
